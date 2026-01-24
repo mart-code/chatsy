417ec1dbb35efdffb08183a81ea9f608c070ac03
mart-code <aamadewoyin@gmail.com>
2026-01-24 13:53:00 +0100

    account changes

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,7 +1,7 @@
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccountKey.json");
+var serviceAccount = require("./firebaseAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
