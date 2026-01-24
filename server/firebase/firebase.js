327c99dfdaf3ad241fe973a1943dd5dca13436a2
mart-code <aamadewoyin@gmail.com>
2026-01-24 14:01:54 +0100

    correct url path for firebase

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,7 +1,7 @@
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebaseAccountKey.json");
+var serviceAccount = require(process.env.FIREBASE_ACCOUNT_URL);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
